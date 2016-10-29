1d51861eb6c164d26281ff5b5f4fa9bb67028f74
Christopher Budd <chris.cyborg@gmail.com>
2016-10-29 15:36:00 -0700

    Project Zomboid server initial commit

---
+++ docker_files/run_server.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+cd /root/mounted
+# Fixes [S_API FAIL] SteamAPI_Init() failed; unable to locate a running instance of Steam, or a local steamclient.dll.
+export HOME="/root"
+file="/root/.steam/sdk32/steamclient.so"
+if ! [ -f "$file" ]
+then
+	mkdir -p ~/.steam/sdk32/
+	ln -s /etc/service/steamcmd/linux32/steamclient.so "$file"
+fi
+
+echo "STARTING SERVER"
+
+exec ./start-server.sh -adminpassword GNUServer -servername GNUServer -nosteam -Xmx1024m -Xms1024m
+
+# Classic Casual
+# # exec ./srcds_run -game csgo -console -usercon +game_type 0 +game_mode 0 +mapgroup mg_active
+# # Classic Competitive
+# #exec ./srcds_run -game csgo -console -usercon +ip 0.0.0.0 +hostport 27015 +game_type 0 +game_mode 1 
+# # Arms Race
+# # exec ./srcds_run -game csgo -console -usercon +game_type 1 +game_mode 0 +mapgroup mg_armsrace
+# # Demolition
+# # exec ./srcds_run -game csgo -console -usercon +game_type 1 +game_mode 1 +mapgroup mg_demolition
+# # Deathmatch
+# # exec ./srcds_run -game csgo -console -usercon +game_type 1 +game_mode 2 +mapgroup mg_allclassic
---
+++ docker_files/steamcmd.cmd
@@ -0,0 +1,19 @@
+// This is an example script
+// see https://developer.valvesoftware.com/wiki/SteamCMD#Automating_SteamCMD
+// for more info
+
+// anonymous login is required in order to download CS:GO
+login anonymous
+// Where all the files will be stored
+// Don't change, unless you know what you are doing
+force_install_dir /root/mounted/
+// See https://developer.valvesoftware.com/wiki/Dedicated_Servers_List#Linux_Dedicated_Servers
+// to find your dedicated server ID
+// 740: Counter-Strike: Global Offensive Dedicated Server
+// 108600: Zomboid server
+app_update 108600
+
+
+//It should always end by quit
+//This script contains only the instructions to download the dedicated server
+quit
